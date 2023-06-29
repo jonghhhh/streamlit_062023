caf3a1266220995aa6f926660ece9609bd62c965
jonghhhh <31147933+jonghhhh@users.noreply.github.com>
2023-06-29 17:46:49 +0900

    Update dataframe.py

--- dataframe.py
+++ dataframe.py
@@ -1,16 +1,13 @@
 import streamlit as st
 import pandas as pd
 
-def search_dataframe(dataframe, keyword):
-    # Filter the DataFrame based on the keyword
-    filtered_df = dataframe[dataframe['Text'].str.contains(keyword, case=False)]
-    return filtered_df
+df = pd.read_excel("동아한겨레1면제목_20130101_20230430.xlsx")
 
 def main():
     st.title("DataFrame Keyword Search")
 
     # Load the DataFrame
-    df = pd.read_excel("동아한겨레1면제목_20130101_20230430.xlsx")
+    #df = pd.read_excel("동아한겨레1면제목_20130101_20230430.xlsx")
 
     # Display the DataFrame
     st.write("Original DataFrame:")
@@ -21,7 +18,14 @@ def main():
 
     # Perform search on button click
     if st.button("Search"):
-        filtered_data = search_dataframe(df, keyword)
+        result = []
+        for i, d in enumerate(df['title']):
+            if keyword in d:
+                result.append(i)
+            else:
+                pass
+    
+        filtered_data = df[df.index.isin(result)]
         st.write("Filtered DataFrame:")
         st.write(filtered_data)
 
