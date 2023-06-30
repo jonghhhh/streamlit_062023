86531b7bbeff82d34f699efa6cb906dd97388abd
jonghhhh <31147933+jonghhhh@users.noreply.github.com>
2023-06-30 10:22:13 +0900

    Update mywordcloud.py

--- mywordcloud.py
+++ mywordcloud.py
@@ -47,8 +47,7 @@ def generate_wordcloud(text):
         colormap = "Accent_r",                                                         # 사용 색상 지정  # https://matplotlib.org/stable/tutorials/colors/colormaps.html
         width=1500, height=1000                                                        # 그림 픽셀
         ).generate_from_frequencies(dict(keywords_all)) 
-    fig, ax = plt.subplots()
-    plt.figure(figsize=(12,8))                                                         # 그림 크기 
+    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(12, 8))    
     plt.imshow(mywordcloud, interpolation='bilinear') 
     plt.axis('off') 
     st.pyplot(fig)
