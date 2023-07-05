e7d4641d6a5fafbb4ccf22a1ca63e1cdaae5ccfa
jonghhhh <31147933+jonghhhh@users.noreply.github.com>
2023-07-05 22:02:15 +0900

    Update mydatabase1.py

--- mydatabase1.py
+++ mydatabase1.py
@@ -1,33 +1,28 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.read_excel("동아한겨레1면제목_20130101_20230430.xlsx")
+data = pd.read_csv("C:/Users/82104/Downloads/전국관광지정보표준데이터.csv", encoding='cp949')  # 검색할 데이터 파일의 경로를 적절히 수정하세요.
 
 def main():
-    st.title("DataFrame Keyword Search")
+    st.title("전국 관광지 정보 검색")
+    st.write("검색 원하는 키워드를 입력해 관련된 관광 정보를 모두 열람하세요")
+    query = st.text_input("검색어:")
 
-    # Load the DataFrame
-    #df = pd.read_excel("동아한겨레1면제목_20130101_20230430.xlsx")
+    result = []
+    for i, d in enumerate(data['관광지소개']):
+        if query in d:
+            result.append(i)
+        else:
+            pass
 
-    # Display the DataFrame
-    st.write("Original DataFrame:")
-    st.write(df)
+    filtered_data = data[data.index.isin(result)]
 
-    # Keyword input
-    keyword = st.text_input("Enter a keyword to search")
-
-    # Perform search on button click
-    if st.button("Search"):
-        result = []
-        for i, d in enumerate(df['title']):
-            if keyword in d:
-                result.append(i)
-            else:
-                pass
-    
-        filtered_data = df[df.index.isin(result)]
-        st.write("Filtered DataFrame:")
-        st.write(filtered_data)
+    if len(filtered_data) > 0:
+        st.write("데이터 일부")
+        st.dataframe(filtered_data)
+    else:
+        st.write("검색어에 부합하는 내용이 없습니다")
 
 if __name__ == "__main__":
     main()
+
