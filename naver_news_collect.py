5ae418ac43d0ec474e4bfbfc168eb7c98eaa4d2a
jonghhhh <31147933+jonghhhh@users.noreply.github.com>
2023-07-21 12:41:50 +0900

    Update naver_news_collect.py

--- naver_news_collect.py
+++ naver_news_collect.py
@@ -59,7 +59,7 @@ def main():
     date_from = st.text_input("검색시작일 입력(20230101 형태로):").strip()
     date_to = st.text_input("검색마지막일 입력(20230101 형태로):").strip()
     to_page = int(float(st.number_input("몇 페이지까지(100페이지까지 가능):", min_value=0, max_value=100, step=1)))
-    save_path = st.text_input("검색 결과 저장 경로(C:/Users/user/Desktop/result.xlsx 방식):").strip()
+    save_path = st.text_input("검색 결과 저장 경로(C://Users/user/Desktop/result.xlsx 방식):").strip()
     if st.button("뉴스 검색 시작"):
         result_file=naver_news_search(query, date_from, date_to, to_page)
         st.dataframe(result_file)
